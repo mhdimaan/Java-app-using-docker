a26ca992a3558cc3a52399d9c0f2d86f51a55489
mhdimaan <muhammed.imaan2023@vitstudent.ac.in>
2026-04-16 12:45:11 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
